bc492b0b60bd7c7511a4de2a98a63650622f3595
rqphy <raphael.ferreira@hetic.net>
2022-04-27 15:37:05 +0200

    :recycle: clean

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-const html = document.documentElement;
-const canvas = document.getElementById("hero");
-const context = canvas.getContext("2d");
+const HTML = document.documentElement
+const CANVAS = document.getElementById("hero")
+const CONTEXT = CANVAS.getContext("2d")
 
-const frameCount = 340;
-const currentFrame = index => (
-  `./assets/IMG_0076${index.toString().padStart(3, '0')}.jpg`
+const frameCount = 340
+
+const currentFrame = index =>
+(
+    `./assets/IMG_0076${index.toString().padStart(3, '0')}.jpg`
 )
 
-const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-  }
+const preloadImages = () =>
+{
+    for (let i = 1; i < frameCount; i++)
+    {
+        const img = new Image()
+        img.src = currentFrame(i)
+    }
 };
 
 const img = new Image()
 img.src = currentFrame(1);
-canvas.width=563;
-canvas.height=1000;
-img.onload=function(){
-  context.drawImage(img, 0, 0);
+CANVAS.width=563;
+CANVAS.height=1000;
+img.onload=function()
+{
+    CONTEXT.drawImage(img, 0, 0)
 }
 
-const updateImage = index => {
-  img.src = currentFrame(index);
-  context.drawImage(img, 0, 0);
+const updateImage = index =>
+{
+    img.src = currentFrame(index)
+    CONTEXT.drawImage(img, 0, 0)
 }
 
-window.addEventListener('scroll', () => {  
-  const scrollTop = html.scrollTop;
-  const maxScrollTop = html.scrollHeight - window.innerHeight;
-  const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(
-    frameCount - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  
-  requestAnimationFrame(() => updateImage(frameIndex + 1))
-});
+window.addEventListener('scroll', () =>
+{  
+    const scrollTop = HTML.scrollTop;
+    const maxScrollTop = HTML.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(
+        frameCount - 1,
+        Math.ceil(scrollFraction * frameCount)
+    )
+
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+})
 
 preloadImages()

4b0569b58f8523d4eeba7bbc48fcacb96ad5ae36
rqphy <raphael.ferreira@hetic.net>
2022-04-28 09:59:37 +0200

    :recycle: small refactor

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// inspi https://codepen.io/j-v-w/pen/ZEbGzyv?editors=0110
 const HTML = document.documentElement
 const CANVAS = document.getElementById("hero")
 const CONTEXT = CANVAS.getContext("2d")
@@ -33,27 +34,13 @@ const updateImage = index =>
     img.src = currentFrame(index)
     CONTEXT.drawImage(img, 0, 0)
 }
-console.log(HTML.scrollHeight, window.innerHeight)
-
-// window.addEventListener('scroll', () =>
-// {  
-//     const scrollTop = HTML.scrollTop;
-//     const maxScrollTop = HTML.scrollHeight - window.innerHeight;
-//     const scrollFraction = scrollTop / maxScrollTop;
-//     const frameIndex = Math.min(
-//         frameCount - 1,
-//         Math.ceil(scrollFraction * frameCount)
-//     )
-
-//     requestAnimationFrame(() => updateImage(frameIndex + 1))
-// })
 
 preloadImages()
 
 const controller = new ScrollMagic.Controller()
 const duration = 3000
 
-const scene = new ScrollMagic.Scene({
+const imgScene = new ScrollMagic.Scene({
     duration,
     triggerElement: INTRO,
     triggerHook: 0
@@ -65,7 +52,7 @@ let scrollPosition = 0
 let accelAmount = 0.1
 let delay = 0
 
-scene.on('update', (_event) =>
+imgScene.on('update', (_event) =>
 {
     scrollPosition = _event.scrollPos
 
